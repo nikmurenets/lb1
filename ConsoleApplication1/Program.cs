4d8072ff4221b0ec894aa053ede7c538462e3dda
nick <murenetsnikita@gmail.com>
2021-09-10 10:36:34 +0300

    third commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Writeline "Hello Snya"
+            Console.Writeline "Hello Snya ilu"
         }
     }
 }
